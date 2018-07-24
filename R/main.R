b3358640088e68907725f0b36e5fdb96caee5f3a
Alex Gyujin Kim <agk2144@columbia.edu>
2018-07-24 15:09:25 -0400

    added cat's for debugging

--- R/main.R
+++ R/main.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
-# Set working directory to directory of this file which is R/
+## Set working directory to directory of this file which is R/
+cat("Setting working directory\n")
 dir_of_this_file <- dirname(sys.frame(1)$ofile) #dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(dir_of_this_file)
 
@@ -8,55 +9,73 @@ source("parameters.R")
 source("functions.R")
 source("make_project.R")
 
-# Argument parser
-## `Rscript main.R /Users/alexkim/Dropbox/Gimelbrant_Lab/datamunge_test_project/plate1.yaml`
-args <- commandArgs(trailingOnly=TRUE)
-yaml_f <- read_arguments(args)
-
-## get plate_name
-plate_name <- tools::file_path_sans_ext(basename(yaml_f))
-
-## Read in parameters into global namespace
-read_parameters(yaml_f)
+## Argument parser
+cat("Parsing arguments\n")
+{
+  ### `Rscript main.R /Users/alexkim/Dropbox/Gimelbrant_Lab/datamunge_test_project/plate1.yaml`
+  args <- commandArgs(trailingOnly=TRUE)
+  yaml_f <- read_arguments(args)
+  
+  ### get plate_name
+  cat(" - Getting plate name\n")
+  plate_name <- tools::file_path_sans_ext(basename(yaml_f))
+  
+  ### Read in parameters into global namespace
+  cat(" - Defining parameters\n")
+  read_parameters(yaml_f)
+}
 
 ## Create project directory structure
-### Globally defines project_dir, outputs_dir
-make_project(output_path, plate_name, overwrite = TRUE)
-
-## Get barcodes
+cat("Creating project folder\n")
 {
-  ### Create master plates list with barcodes
-  barcode_maps_f <- file.path(barcode_maps_path, paste0(barcode_maps, ".xlsx"))
-  plates.bc <- create_plates(plate_ids, barcode_maps_f)
+  ### Globally defines project_dir, outputs_dir
+  cat(" - Getting project paths\n")
+  make_project(output_path, plate_name, overwrite = TRUE) 
 }
 
-## Get Construct/Perturbation ID's
+## Create plates
+cat("Creating master plates\n")
 {
-  constructs_f <- file.path(constructs_map_path, paste0(constructs_maps, ".xlsx"))
-  constructs_fo <- file.path(outputs_dir, paste0(constructs_maps, "_w_ids.xlsx"))
-  WRITE_CONSTRUCT_FILE <- TRUE
+  ### Get barcodes
+  cat(" - Getting barcodes\n")
+  {
+    #### Create master plates list with barcodes
+    cat("  - Populate plates with barcodes\n")
+    barcode_maps_f <- file.path(barcode_maps_path, paste0(barcode_maps, ".xlsx"))
+    plates.bc <- create_plates(plate_ids, barcode_maps_f)
+  }
   
-  constructs_list <- get_constructs(plate_ids, constructs_f, constructs_fo, WRITE_CONSTRUCT_FILE) # Region
+  ### Get Construct/Perturbation ID's
+  cat(" - Getting perturbations\n")
+  {
+    constructs_f <- file.path(constructs_map_path, paste0(constructs_maps, ".xlsx"))
+    constructs_fo <- file.path(outputs_dir, paste0(constructs_maps, "_w_ids.xlsx"))
+    WRITE_CONSTRUCT_FILE <- TRUE
+    
+    constructs_list <- get_constructs(plate_ids, constructs_f, constructs_fo, WRITE_CONSTRUCT_FILE) # Region
+    
+    #### Add construct id data to plates list
+    cat("  - Populate plates with perturbations\n")
+    plates.bc.cst <- add_lists_to_plates(plates.bc, "construct", constructs_list)
+  }
   
-  ### Add construct id data to plates list
-  plates.bc.cst <- add_lists_to_plates(plates.bc, "construct", constructs_list)
-}
-
-
-## Get Cell Quality Data
-
-### Get clean data from Shiny App
-{
-  cell_quals_f <- file.path(cell_quals_path, cell_quals)
-  
-  cell_quals_list <- get_cell_qualities(plate_ids, cell_quals_f)
-  
-  ### Add cell_quality data to plates list
-  plates.bc.cst.cq <- add_lists_to_plates(plates.bc.cst, "cell_quality", cell_quals_list)
+  ### Get Cell Viability Data
+  cat(" - Getting cell viability data\n")
+  {
+    #### Get clean data from Shiny App
+    cell_quals_f <- file.path(cell_quals_path, cell_quals)
+    
+    cell_quals_list <- get_cell_qualities(plate_ids, cell_quals_f)
+    
+    #### Add cell_quality data to plates list
+    cat("  - Populate plates with cell viabilities\n")
+    plates.bc.cst.cq <- add_lists_to_plates(plates.bc.cst, "cell_quality", cell_quals_list)
+  }
 }
 
 
 ## Write well YAML
+cat("Writing master plates to yaml\n")
 {
   WRITE_WELLS_FILE <- TRUE
   WRITE_PRINTSHEET_HELPER <- TRUE
@@ -66,6 +85,7 @@ make_project(output_path, plate_name, overwrite = TRUE)
 
 
 ## Write config YAML
+cat("Writing configuration yaml\n")
 {
   yaml_fo <- file.path(project_dir, "config", paste0(run, ".yaml"))
   
@@ -73,8 +93,14 @@ make_project(output_path, plate_name, overwrite = TRUE)
 }
 
 ## Run Step 1 - Alignment and Step 2 - Count
+cat("Running perl scripts\n")
 {
+  ### Step 1
+  cat(" - Aligning\n")
   run_s1(yaml_fo, doS1)
+  
+  ### Step 2
+  cat(" - Counting\n")
   run_s2(yaml_fo, doS2)
 }
 
